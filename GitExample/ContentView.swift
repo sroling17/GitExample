4c0c9753f0fbae3c5c5cfe1e93c8c2210ffc8caa
Simon Roling <s014931@students.lmsd.org>
2021-03-11 10:59:11 -0500

    Initial Commit

---
+++ GitExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitExample
+//
+//  Created by Simon Roling (student LM) on 3/11/21.
+//  Copyright © 2021 Simon Roling (Student LM). All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
